95b6b5579de9ce8f8ac33b3e4751cc376e3c6ad3
nZibben88 <126527349+nZibben88@users.noreply.github.com>
2023-06-28 16:53:30 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = '6253047238:AAH-C7QoqFYSpJhVUQcBmwK_JDqJy_Cca20'
+
+keys = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'Доллар': 'USD',
+    'Евро': 'EUR',
+    'Рубль': 'RUB'
+}
